d7f7cb8292ff7e95a9e370ab638afdcd8cd384d2
rjfenton <rjfenton@ebrlabs.com>
2018-12-10 11:55:33 -0700

    Fix routes import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRouting } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting,
   ],
   providers: [],
   bootstrap: [AppComponent]
